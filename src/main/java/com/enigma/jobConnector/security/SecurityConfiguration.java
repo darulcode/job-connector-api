a04d27f4e956259a514d6699937b40944e02a9a6
DarulCode <ahmaddarul72@gmail.com>
2024-12-19 00:43:39 +0700

    chore : update cors

--- src/main/java/com/enigma/jobConnector/security/SecurityConfiguration.java
+++ src/main/java/com/enigma/jobConnector/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.enigma.jobConnector.security;
 
+
 import jakarta.servlet.DispatcherType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -11,11 +12,6 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -32,7 +28,6 @@ public class SecurityConfiguration {
         return http
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .exceptionHandling(config -> {
                     config.accessDeniedHandler(customAccessDeniedHandler);
                     config.authenticationEntryPoint(customAuthEntryPoint);
@@ -58,20 +53,4 @@ public class SecurityConfiguration {
                 .build();
     }
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of(
-                "http://localhost:3000",
-                "http://web.enigjob.my.id",
-                "http://152.42.224.141:3000"
-        ));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
 }
