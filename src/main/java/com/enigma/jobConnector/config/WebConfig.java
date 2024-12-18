26052dc7cb6eddb52a52b7de7cff5e88ef321e35
DarulCode <ahmaddarul72@gmail.com>
2024-12-18 23:34:06 +0700

    chore : add cors

--- src/main/java/com/enigma/jobConnector/config/WebConfig.java
+++ src/main/java/com/enigma/jobConnector/config/WebConfig.java
@@ -1,5 +1,5 @@
 package com.enigma.jobConnector.config;
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://web.enigjob.my.id")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
